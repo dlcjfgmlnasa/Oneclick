63c63b1f5d9a96f05c4890f9b0a5573309edc80e
이철희 <dlcjfgmlnasa@naver.com>
2024-05-04 00:19:31 +0900

    CORS 추가

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,7 +146,8 @@ SIMPLE_JWT = {
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
                          'http://localhost:3000',
                          'http://localhost:5000',
-                         'http://127.0.0.1:5000']
+                         'http://127.0.0.1:5000',
+                         'http://34.64.120.63']
 
 
 CORS_ALLOW_CREDENTIALS = True
