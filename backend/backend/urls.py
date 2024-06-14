412517fcd3e0155780b74319a591339de7dd438b
이철희 <dlcjfgmlnasa@naver.com>
2024-06-14 14:14:34 +0900

    Update Media URL

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from .views import MyTokenObtainPairView
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -30,4 +32,4 @@ urlpatterns = [
     path('api/v1/exp/', include('experiments.urls')),
     path('api/v1/ecg/', include('ecg.urls')),
     path('api/v1/eeg/', include('eeg.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
