dfb2b38553c834f32c3ae69db7937b63e6ae1874
Choel-Hui Lee <dlcjfgmlnasa28@korea.ac.kr>
2025-01-03 17:20:27 +0900

    Update Pagination Size

--- backend/experiments/views.py
+++ backend/experiments/views.py
@@ -16,7 +16,7 @@ from django.core.files.base import ContentFile
 
 
 class ExperimentsPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 15
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
